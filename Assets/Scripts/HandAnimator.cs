eabbeeb202d7873c96b0c9c46383674abdfa347f
Valen Chang <yenyu.chang@techmdinc.com>
2025-01-25 20:41:29 -0800

    hand animation and 2D BGM working

---
+++ Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimator : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty triggerAction; //activate
+    [SerializeField] private InputActionProperty gripAction; // select
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = triggerAction.action.ReadValue<float>();
+        float gripValue = gripAction.action.ReadValue<float>();
+
+        animator.SetFloat("Trigger", triggerValue);
+        animator.SetFloat("Grip", gripValue);
+
+    }
+}
