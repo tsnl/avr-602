140925d504bc71107a3a69f0fa103f11fd6b284e
Nikhil Idiculla <nikhilidiculla@gmail.com>
2025-02-16 21:45:42 -0800

    Add glow to baseball bat on gaze

---
+++ Assets/Scripts/BaseballSceneManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseballSceneManager : MonoBehaviour
+{
+    private BatState batState;
+
+    public GameObject batGameObject;
+    public GameObject batGazeInteractibleMeshGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        batState = BatState.Spawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnBatGrabbed()
+    {
+        Debug.Log("Bat grabbed");
+        batState = BatState.Grabbed;
+
+        // Unconditionally disable the bat mesh renderer when bat is grabbed.
+        batGazeInteractibleMeshGameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void OnBatReleased()
+    {
+        Debug.Log("Bat released");
+        batState = BatState.Spawn;
+    }
+
+    public void OnBatGazeEnter()
+    {
+        if (batState == BatState.Spawn)
+        {
+            Debug.Log("Bat gaze enter");
+            batGazeInteractibleMeshGameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
+    public void OnBatGazeLeave()
+    {
+        // Unconditionally disable the bat mesh renderer when gaze leaves or when bat is grabbed.
+        batGazeInteractibleMeshGameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
+
+enum BatState
+{
+    Spawn,
+    Grabbed,
+}
